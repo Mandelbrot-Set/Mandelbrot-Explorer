59a1a7e8da0999225613cd6add3b223411925d12
xiaoyongwang <2411148291@qq.com>
2019-05-06 21:55:14 +0800

    refined by PE8

--- framework.py
+++ framework.py
@@ -1,6 +1,4 @@
-import os
 import argparse
-import sys
 import math
 import time
 import random
@@ -10,59 +8,62 @@ from mandelbrot import Mandelbrot
 
 
 class Framework(Frame):
-    def __init__(self, parent, h, x=-0.75, y=0, m=1.5, iterations=None, imgWidth=None, imgHeight=None, save=False, multi=True):
+    def __init__(self, parent, h, x=-0.75, y=0, m=1.5, iterations=None, img_width=None,
+                 img_height=None, save=False, multi=True):
         Frame.__init__(self, parent)
         self.parent = parent
         self.parent.title("Mandelbrot")
         self.pack(fill=BOTH, expand=1)
         self.canvas = Canvas(self)
-
-        if None in {imgWidth, imgHeight}:
-            imgWidth, imgHeight = h, h
-        if imgWidth > imgHeight:
-            ratio = imgHeight/imgWidth
+        self.palette = None
+        self.background = None
+        if None in {img_width, img_height}:
+            img_width, img_height = h, h
+        if img_width > img_height:
+            ratio = img_height/img_width
             self.canvasW, self.canvasH = h, round(h*ratio)
         else:
-            ratio = imgWidth/imgHeight
+            ratio = img_width/img_height
             self.canvasW, self.canvasH = round(h*ratio), h
 
-        self.fractal = Mandelbrot(self.canvasW, self.canvasH, x=x, y=y, m=m, iterations=iterations, w=imgWidth, h=imgHeight, multi=multi)
-        self.setPalette()
+        self.fractal = Mandelbrot(self.canvasW, self.canvasH, x=x, y=y, m=m, iterations=iterations,
+                                  w=img_width, h=img_height, multi=multi)
+        self.set_palette()
         self.pixelColors = []
         self.img = None
         self.save = save
         self.draw()
 
-        parent.bind("<Button-1>", self.zoomIn)
-        parent.bind("<Button-3>", self.zoomOut)
-        parent.bind("<Control-1>", self.shiftView)
-        parent.bind("<Control-3>", self.changePalette)
-        parent.bind("<Button-2>", self.saveImage)
+        parent.bind("<Button-1>", self.zoom_in)
+        parent.bind("<Button-3>", self.zoom_out)
+        parent.bind("<Control-1>", self.shift_view)
+        parent.bind("<Control-3>", self.change_palette)
+        parent.bind("<Button-2>", self.save_image)
 
-    def zoomIn(self, event):
-        self.fractal.zoomIn(event)
+    def zoom_in(self, event):
+        self.fractal.zoom_in(event)
         self.draw()
 
-    def zoomOut(self, event):
-        self.fractal.zoomOut(event)
+    def zoom_out(self, event):
+        self.fractal.zoom_out(event)
         self.draw()
 
-    def shiftView(self, event):
-        self.fractal.shiftView(event)
+    def shift_view(self, event):
+        self.fractal.shift_view(event)
         self.draw()
 
     def draw(self):
         print('-' * 20)
         start = time.time()
-        self.fractal.getPixels()
-        self.getColors()
-        self.drawPixels()
+        self.fractal.get_pixels()
+        self.get_colors()
+        self.draw_pixels()
         self.canvas.create_image(0, 0, image=self.background, anchor=NW)
         self.canvas.pack(fill=BOTH, expand=1)
         print("Process took {} seconds".format(round(time.time()-start, 2)))
         print("Current coordinates (x, y, m): {}, {}, {}".format(self.fractal.xCenter, self.fractal.yCenter, self.fractal.delta))
 
-    def setPalette(self):
+    def set_palette(self):
         palette = [(0, 0, 0)]
         redb = 2 * math.pi / (random.randint(0, 128) + 128)
         redc = 256 * random.random()
@@ -77,32 +78,32 @@ class Framework(Frame):
             palette.append((r, g, b))
         self.palette = palette
 
-    def changePalette(self, event):
-        self.setPalette()
+    def change_palette(self, event):
+        self.set_palette()
         self.pixelColors = []
-        self.getColors()
-        self.drawPixels()
+        self.get_colors()
+        self.draw_pixels()
         self.canvas.create_image(0, 0, image=self.background, anchor=NW)
         self.canvas.pack(fill=BOTH, expand=1)
 
-    def getColors(self):
-        pixelColors = []
+    def get_colors(self):
+        pixel_colors = []
         for p in self.fractal.pixels:
-            pixelColors.append(self.palette[p[2] % 256])
-        self.pixelColors = pixelColors
+            pixel_colors.append(self.palette[p[2] % 256])
+        self.pixelColors = pixel_colors
 
-    def drawPixels(self):
+    def draw_pixels(self):
         img = Image.new('RGB', (self.fractal.w, self.fractal.h), "black")
         pixels = img.load()  # create the pixel map
         for index, p in enumerate(self.fractal.pixels):
             pixels[int(p[0]), int(p[1])] = self.pixelColors[index]
         self.img = img
         if self.save:
-            self.saveImage(None)
+            self.save_image(None)
         photoimg = ImageTk.PhotoImage(img.resize((self.canvasW, self.canvasH)))
         self.background = photoimg
 
-    def saveImage(self, event):
+    def save_image(self, event):
         self.img.save("output/{}.png".format(time.strftime("%Y-%m-%d-%H:%M:%S")), "PNG", optimize=True)
 
 
@@ -114,7 +115,8 @@ def main():
     master = Tk()
     height = round(master.winfo_screenheight()*0.9)
     parser = argparse.ArgumentParser(description='Generate the Mandelbrot set')
-    parser.add_argument('-i', '--iterations', type=int, help='The number of iterations done for each pixel. Higher is more accurate but slower.')
+    parser.add_argument('-i', '--iterations', type=int, help='The number of iterations done for each pixel. '
+                                                             'Higher is more accurate but slower.')
     parser.add_argument('-x', type=float, help='The x-center coordinate of the frame.')
     parser.add_argument('-y', type=float, help='The y-center coordinate of the frame.')
     parser.add_argument('-m', '--magnification', type=float, help='The magnification level of the frame.')
@@ -125,12 +127,12 @@ def main():
     args = parser.parse_args()
     if None not in [args.x, args.y, args.magnification]:
         render = Framework(master, height, x=args.x, y=args.y, m=args.magnification, multi=args.noMulti,
-                           iterations=args.iterations, imgWidth=args.width, imgHeight=args.height, save=args.save)
+                           iterations=args.iterations, img_width=args.width, img_height=args.height, save=args.save)
     else:
         if not all(arg is None for arg in [args.x, args.y, args.magnification]):
             print("Arguments ignored. Please provide all of x, y, & m.")
         render = Framework(master, height, multi=args.noMulti, iterations=args.iterations,
-                           imgWidth=args.width, imgHeight=args.height, save=args.save)
+                           img_width=args.width, img_height=args.height, save=args.save)
     master.geometry("{}x{}".format(render.canvasW, render.canvasH))
     master.mainloop()
 
--- mandelbrot.py
+++ mandelbrot.py
@@ -1,18 +1,30 @@
 from multiprocessing import Pool
-import sys
-import os
 
 
-class Mandelbrot():
-    def __init__(self, canvasW, canvasH, x=-0.75, y=0, m=1.5, iterations=None, w=None, h=None, zoomFactor=0.1, multi=True):
-        self.w, self.h = (round(canvasW*0.9), round(canvasH*0.9)) if None in {w, h} else w, h
+class Mandelbrot:
+    def __init__(self, canvas_w, canvas_h, x=-0.75, y=0, m=1.5, iterations=None,
+                 w=None, h=None, zoom_factor=0.1, multi=True):
+        """
+        初始化实例
+        :param canvas_w:
+        :param canvas_h:
+        :param x: 帧的中心坐标点 x 坐标
+        :param y: 帧的中心坐标点 y 坐标
+        :param m: 放大比例
+        :param iterations: 迭代次数，次数越高越精确，但是速度慢
+        :param w: 图像的宽
+        :param h: 图像的高
+        :param zoom_factor: 缩放系数
+        :param multi: 多进程标记
+        """
+        self.w, self.h = (round(canvas_w*0.9), round(canvas_h*0.9)) if None in {w, h} else w, h
         self.iterations = 200 if iterations is None else iterations
         self.xCenter, self.yCenter = x, y
-        if canvasW > canvasH:
-            self.xDelta = m/(canvasH/canvasW)
+        if canvas_w > canvas_h:
+            self.xDelta = m/(canvas_h/canvas_w)
             self.yDelta = m
         else:
-            self.yDelta = m/(canvasW/canvasH)
+            self.yDelta = m/(canvas_w/canvas_h)
             self.xDelta = m
         self.delta = m
         self.multi = multi
@@ -20,12 +32,13 @@ class Mandelbrot():
         self.xmax = x + self.xDelta
         self.ymin = y - self.yDelta
         self.ymax = y + self.yDelta
-        self.zoomFactor = zoomFactor
-        self.yScaleFactor = self.h/canvasH
-        self.xScaleFactor = self.w/canvasW
+        self.zoomFactor = zoom_factor
+        self.yScaleFactor = self.h/canvas_h
+        self.xScaleFactor = self.w/canvas_w
         self.c, self.z = 0, 0
+        self.pixels = []
 
-    def shiftView(self, event):
+    def shift_view(self, event):
         self.xCenter = translate(event.x*self.xScaleFactor, 0, self.w, self.xmin, self.xmax)
         self.yCenter = translate(event.y*self.yScaleFactor, self.h, 0, self.ymin, self.ymax)
         self.xmax = self.xCenter + self.xDelta
@@ -33,7 +46,7 @@ class Mandelbrot():
         self.xmin = self.xCenter - self.xDelta
         self.ymin = self.yCenter - self.yDelta
 
-    def zoomOut(self, event):
+    def zoom_out(self, event):
         self.xCenter = translate(event.x*self.xScaleFactor, 0, self.w, self.xmin, self.xmax)
         self.yCenter = translate(event.y*self.yScaleFactor, self.h, 0, self.ymin, self.ymax)
         self.xDelta /= self.zoomFactor
@@ -44,7 +57,7 @@ class Mandelbrot():
         self.xmin = self.xCenter - self.xDelta
         self.ymin = self.yCenter - self.yDelta
 
-    def zoomIn(self, event):
+    def zoom_in(self, event):
         self.xCenter = translate(event.x*self.xScaleFactor, 0, self.w, self.xmin, self.xmax)
         self.yCenter = translate(event.y*self.yScaleFactor, self.h, 0, self.ymin, self.ymax)
         self.xDelta *= self.zoomFactor
@@ -55,36 +68,36 @@ class Mandelbrot():
         self.xmin = self.xCenter - self.xDelta
         self.ymin = self.yCenter - self.yDelta
 
-    def getPixels(self):
+    def get_pixels(self):
         coordinates = []
         for x in range(self.w):
             for y in range(self.h):
                 coordinates.append((x, y))
         if self.multi:
             pool = Pool()
-            self.pixels = pool.starmap(self.getEscapeTime, coordinates)
+            self.pixels = pool.starmap(self.get_escape_time, coordinates)
             pool.close()
             pool.join()
         else:
             print("Using 1 core...")
             pixels = []
             for coord in coordinates:
-                pixels.append(self.getEscapeTime(coord[0], coord[1]))
+                pixels.append(self.get_escape_time(coord[0], coord[1]))
             self.pixels = pixels
 
-    def getEscapeTime(self, x, y):
+    def get_escape_time(self, x, y):
         re = translate(x, 0, self.w, self.xmin, self.xmax)
         im = translate(y, 0, self.h, self.ymax, self.ymin)
         z, c = complex(re, im), complex(re, im)
         for i in range(1, self.iterations):
             if abs(z) > 2:
-                return (x, y, i)
+                return x, y, i
             z = z*z + c
-        return (x, y, 0)
+        return x, y, 0
 
 
-def translate(value, leftMin, leftMax, rightMin, rightMax):
-    leftSpan = leftMax - leftMin
-    rightSpan = rightMax - rightMin
-    valueScaled = float(value - leftMin) / float(leftSpan)
-    return rightMin + (valueScaled * rightSpan)
+def translate(value, left_min, left_max, right_min, right_max):
+    left_span = left_max - left_min
+    right_span = right_max - right_min
+    value_scaled = float(value - left_min) / float(left_span)
+    return right_min + (value_scaled * right_span)
