ee15544032f49b98841399c2177d3df8dbd1e21c
enimgawxy <1480973357@qq.com>
2019-05-21 17:44:28 +0800

    5.21.evening

--- mandelbrot_plotly.py
+++ mandelbrot_plotly.py
@@ -1,10 +1,10 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import numpy as np
 import plotly.graph_objs as go
 from dash.dependencies import Output, Input
-from numba import jit
+import time
+from utils import *
 
 iw = 1500
 ih = 1500
@@ -22,25 +22,34 @@ app = dash.Dash()
 app.config['suppress_callback_exceptions'] = True
 
 
-@jit
-def mandelbrot(c, maxiter, threshold=2):
-    z_ = c
-    for n in range(maxiter):
-        if abs(z_) > threshold:
-            return n
-        z_ = z_ * z_ + c
-    return 0
-
-
-@jit
 def mandelbrot_set(xmin, xmax, ymin, ymax, maxiter=250, width=iw, height=ih):
-    r1 = np.linspace(xmin, xmax, width)
-    r2 = np.linspace(ymin, ymax, height)
-    n3 = np.empty((width, height))
-    for i in range(width):
-        for j in range(height):
-            n3[i, j] = mandelbrot(r1[i] + 1j * r2[j], maxiter)
-    return r1, r2, n3
+    re = np.linspace(xmin, xmax, width, dtype=np.float64)
+    im = np.linspace(ymin, ymax, height, dtype=np.float64)
+    c = re + im[:, None]*1j
+
+    n3 = mandelbrot_numpy(c, maxiter)
+
+    return re, im, n3.T
+
+# @jit
+# def mandelbrot(c, maxiter, threshold=2):
+#     z_ = c
+#     for n in range(maxiter):
+#         if abs(z_) > threshold:
+#             return n
+#         z_ = z_ * z_ + c
+#     return 0
+#
+#
+# @jit
+# def mandelbrot_set(xmin, xmax, ymin, ymax, maxiter=250, width=iw, height=ih):
+#     r1 = np.linspace(xmin, xmax, width)
+#     r2 = np.linspace(ymin, ymax, height)
+#     n3 = np.empty((width, height))
+#     for i in range(width):
+#         for j in range(height):
+#             n3[i, j] = mandelbrot(r1[i] + 1j * r2[j], maxiter)
+#     return r1, r2, n3
 
 
 x, y, z = mandelbrot_set(-2.0, 0.5, -1.25, 1.25)
@@ -142,7 +151,10 @@ def display_selected_data(iterations, relayoutData):
         ymin = relayoutData['yaxis.range[0]']
         ymax = relayoutData['yaxis.range[1]']
 
+    start_i = time.time()
     x_, y_, z_ = mandelbrot_set(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, maxiter=iterations)
+    print("迭代执行时间 {} 秒".format(round(time.time() - start_i, 2)))
+    start_i = time.time()
     trace_ = go.Heatmap(x=x_,
                         y=y_,
                         z=z_.T)
@@ -161,7 +173,7 @@ def display_selected_data(iterations, relayoutData):
     )
 
     fig_ = go.Figure(data=data_, layout=layout_)
-
+    print("渲染执行时间 {} 秒".format(round(time.time() - start_i, 2)))
     return fig_
 
 
